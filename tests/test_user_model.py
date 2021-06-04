a5cff5a1e66fe8a71034882c82be23bf2744244e
chenzikang <chenzikangyoder@gmail.com>
2021-06-04 22:07:40 +0800

    用户角色

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -9,9 +9,9 @@ import unittest
 from flask import url_for
 from wtforms import ValidationError
 
-from app.auth.forms import RegistrationForm
-from app.models import User, Role
 from app import create_app, db
+from app.auth.forms import RegistrationForm
+from app.models import User, AnonymousUser, Role, Permission
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -20,14 +20,18 @@ class UserModelTestCase(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
+        Role.insert_roles()
         user = User(email="chenzikangyoder@gmail.com", username="as")
         db.session.add(user)
+        db.session.commit()
         self.client = self.app.test_client()
+
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
+
 
     def test_register(self):
         response = self.client.post(url_for('auth.register'), data={
@@ -37,12 +41,14 @@ class UserModelTestCase(unittest.TestCase):
         })
         self.assertTrue(response.status_code == 200)
 
+
     def test_login(self):
         response = self.client.post(url_for('auth.login'), data={
             'email': 'chenzikangyoder@gmail.com',
             'password': 'as'
         })
         self.assertTrue(response.status_code == 200)
+
 
     def test_validata_email(self):
         with self.app.test_request_context():
@@ -51,6 +57,7 @@ class UserModelTestCase(unittest.TestCase):
                 form.email.data = "chenzikangyoder@gmail.com"
                 form.validate_email(form.email)
 
+
     def test_validata_username(self):
         with self.app.test_request_context():
             form = RegistrationForm()
@@ -58,21 +65,36 @@ class UserModelTestCase(unittest.TestCase):
                 form.username.data = "as"
                 form.validate_username(form.username)
 
+
     def test_password_setter(self):
         u = User(password='cat')
         self.assertTrue(u.password_hash is not None)
+
 
     def test_no_password_getter(self):
         u = User(password='cat')
         with self.assertRaises(AttributeError):
             u.password
 
+
     def test_password_verification(self):
         u = User(password='cat')
         self.assertTrue(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
 
+
     def test_password_salts_are_random(self):
         u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+
+    def test_roles_and_permissions(self):
+        u = User(email='hfuisdh@gmail.com', password='as')
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertFalse(u.can(Permission.UPLOAD))
+
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
